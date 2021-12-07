a48b95cdc2dffea280b7f32557f7ced040e186c0
Priyanka-Kasireddy <priyankakasireddy2000@msitprogram.net>
2021-12-07 15:44:34 +0530

    enqueue

--- Queue.java
+++ Queue.java
@@ -5,35 +5,42 @@ import java.io.*;
 import java.util.*;
 
 class Queue {
-
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
 
     public Queue() {
-
+        stack1 = new Stack<Integer> ();
+        stack2 = new Stack<Integer> ();
     }
 
     // Push element x to the back of queue.
+    //Time Complexity: O(N),  space complexity: O(1)
     public void enqueue(int x) {
-
+        while(!stack1.isEmpty())
+        stack2.push(stack1.pop());
+        stack2.push(x);
+        while(!stack2.isEmpty())
+        stack1.push(stack2.pop());
     }
 
     // Removes the element from in front of queue.
     public int dequeue() {
-
+        return 0;
     }
     
     // Get the front element.
     public int peek() {
-
+        return 0;
     }
     
     // Return whether the queue is empty.
     public boolean empty() {
-
+        return false;
     }
 
     // Return the number of elements in queue.
     public boolean size() {
-
+        return false;
     }
     
     public static void main(String[] args) {
